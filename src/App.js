e7004e7d3a11111c3250d504bdf29b65f30110e3
JorgeZarlenga <jorge.zarlenga@outlook.com>
2020-08-27 15:35:37 -0300

    Utilização de estado e imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 // Arquivo principal
 
-import React from 'react';
+import React, {useState} from 'react';
 
 import Header from './components/Header';   
 
 /*
     - Componente
     - Propriedade: Alguma propriedade que se passa do componente pai para o filho
-    - Estado
-
+    - Estado & Imutabilidade (garante performance mesmo com muitos dados - não se pode alterar diretamente as variáveis, apenas recriar)
+    // Método push não respeita imutabilidade (evitar no react)
 */
 
 
 function App()
 {
+
+    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+    //useState retorna um array com 2 posições:
+
+    //1 - Variável com seu valor inicial
+    //2 - Função para atualizar esse valor
+
+    function handleAddProject()
+    {
+        // projects.push(`Novo projeto ${Date.now()}`);
+
+        setProjects([...projects, `Novo projeto ${Date.now()}`]); // Spread operator que copia o que há em projects
+
+        console.log(projects);
+    }
+
+    // ou usar tag vazia:
+    return (
+        <>
+          
+            <Header title="Projects" />
+
+            <ul>
+                {projects.map(project => <li key = {project}>{project}</li>)}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+        </>
+    );
+
     //return <h1>Hello GoStack</h1>;
 
     // É preciso no react ter algo em volta para repetição (colocar div)
@@ -26,24 +57,7 @@ function App()
     );
     */
 
-    // ou usar tag vazia:
-    return (
-        <>
-            <Header title="Homepage">
-                <ul>
-                    <li>Homepage</li>
-                    <li>Projects</li>
-                </ul>
-            </Header>
-            <Header title="Projects">
-                <ul>
-                    <li>Homepage</li>
-                    <li>Projects</li>
-                    <li>Login</li>
-                </ul>
-            </Header>
-        </>
-    );
+    
     
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function Header({title, children}) { // ou usar desustrutração com {title} ao invés de props, assim como dentro de h1, que ia propos.title
+export default function Header({title}) { // ou usar desustrutração com {title} ao invés de props, assim como dentro de h1, que ia propos.title
     return(
         <header>
             <h1>{title} </h1>
 
-            {children}
+          
         </header>
     )
 }
